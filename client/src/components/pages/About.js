b00aea05116ef5bef8b03c344088668c3f38a292
Anastasiia Kozlova <anastasiia.sh.v@gmail.com>
2020-08-27 09:56:04 +0200

    front-end setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this App</h1>
+            <p> This is a full stack react app for keeping contacts</p>
+        </div>
+    )
+}
+
+export default About
